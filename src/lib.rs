8463f7aca05640a8a9c2d84fb7a5ea9942b31468
Jodus-Melodus <philipgricius@gmail.com>
2025-07-07 14:50:19 +0200

    add missing tests

--- src/lib.rs
+++ src/lib.rs
@@ -34,7 +34,7 @@ mod matrix_tests {
     }
 
     #[test]
-    fn test_scalar_addition() {
+    fn test_scalar_matrix_addition() {
         let matrix = Matrix::new([[4, 3, -2], [0, 2, 1], [0, 3, 1]]);
         let scalar = 5;
         let actual = Matrix::new([[9, 8, 3], [5, 7, 6], [5, 8, 6]]);
@@ -43,7 +43,7 @@ mod matrix_tests {
     }
 
     #[test]
-    fn test_scalar_subtraction() {
+    fn test_scalar_matrix_subtraction() {
         let matrix = Matrix::new([[4, 3, -2], [0, 2, 1], [0, 3, 1]]);
         let scalar = 5;
         let actual = Matrix::new([[-1, -2, -7], [-5, -3, -4], [-5, -2, -4]]);
@@ -52,7 +52,7 @@ mod matrix_tests {
     }
 
     #[test]
-    fn test_scalar_multiplication() {
+    fn test_scalar_matrix_multiplication() {
         let matrix = Matrix::new([[4.0, 3.0, -2.0], [0.0, 2.0, 1.0], [0.0, 3.0, 1.0]]);
         let scalar = 5.0;
         let actual = Matrix::new([[20.0, 15.0, -10.0], [0.0, 10.0, 5.0], [0.0, 15.0, 5.0]]);
@@ -61,7 +61,7 @@ mod matrix_tests {
     }
 
     #[test]
-    fn test_scalar_division() {
+    fn test_scalar_matrix_division() {
         let matrix = Matrix::new([[4.0, 3.0, -2.0], [0.0, 2.0, 1.0], [0.0, 3.0, 1.0]]);
         let scalar = 5.0;
         let actual = Matrix::new([
@@ -74,7 +74,7 @@ mod matrix_tests {
     }
 
     #[test]
-    fn test_scalar_addition_assign() {
+    fn test_scalar_matrix_addition_assign() {
         let mut matrix = Matrix::new([[4, 3, -2], [0, 2, 1], [0, 3, 1]]);
         let scalar = 5;
         matrix += scalar;
@@ -84,7 +84,7 @@ mod matrix_tests {
     }
 
     #[test]
-    fn test_scalar_subtraction_assign() {
+    fn test_scalar_matrix_subtraction_assign() {
         let mut matrix = Matrix::new([[4, 3, -2], [0, 2, 1], [0, 3, 1]]);
         let scalar = 5;
         matrix -= scalar;
@@ -94,7 +94,7 @@ mod matrix_tests {
     }
 
     #[test]
-    fn test_scalar_multiplication_assign() {
+    fn test_scalar_matrix_multiplication_assign() {
         let mut matrix = Matrix::new([[4.0, 3.0, -2.0], [0.0, 2.0, 1.0], [0.0, 3.0, 1.0]]);
         let scalar = 5.0;
         matrix *= scalar;
@@ -104,7 +104,7 @@ mod matrix_tests {
     }
 
     #[test]
-    fn test_scalar_division_assign() {
+    fn test_scalar_matrix_division_assign() {
         let mut matrix = Matrix::new([[4.0, 3.0, -2.0], [0.0, 2.0, 1.0], [0.0, 3.0, 1.0]]);
         let scalar = 5.0;
         matrix /= scalar;
@@ -225,5 +225,78 @@ mod vector_tests {
         let vec3 = vec1 + vec2;
 
         assert_eq!(vec3, Vector::new([1, 2, -1]));
+    }
+
+    #[test]
+    fn test_vector_subtraction() {
+        let vec1 = Vector::new([4, 0, -1]);
+        let vec2 = Vector::new([-3, 2, 0]);
+        let vec3 = vec1 - vec2;
+
+        assert_eq!(vec3, Vector::new([7, -2, -1]));
+    }
+
+    #[test]
+    fn test_scalar_vector_addition() {
+        let vec1 = Vector::new([3, -3, 9]);
+        let res = vec1 + 5;
+
+        assert_eq!(res, Vector::new([8, 2, 14]));
+    }
+
+    #[test]
+    fn test_scalar_vector_addition_assign() {
+        let mut vec1 = Vector::new([3, -3, 9]);
+        vec1 += 5;
+
+        assert_eq!(vec1, Vector::new([8, 2, 14]));
+    }
+
+    #[test]
+    fn test_scalar_vector_subtraction() {
+        let vec1 = Vector::new([3, -3, 9]);
+        let res = vec1 - 5;
+
+        assert_eq!(res, Vector::new([-2, -8, 4]));
+    }
+
+    #[test]
+    fn test_scalar_vector_subtraction_assign() {
+        let mut vec1 = Vector::new([3, -3, 9]);
+        vec1 -= 5;
+
+        assert_eq!(vec1, Vector::new([-2, -8, 4]));
+    }
+
+    #[test]
+    fn test_scalar_vector_multiplication() {
+        let vec1 = Vector::new([-3, 9, 2]);
+        let res = vec1 * 3;
+
+        assert_eq!(res, Vector::new([-9, 27, 6]));
+    }
+
+    #[test]
+    fn test_scalar_vector_multiplication_assign() {
+        let mut vec1 = Vector::new([3, -3, 9]);
+        vec1 *= 3;
+
+        assert_eq!(vec1, Vector::new([-9, 27, 6]));
+    }
+
+    #[test]
+    fn test_scalar_vector_division() {
+        let vec1 = Vector::new([-3, 9, 2]);
+        let res = vec1 / 3;
+
+        assert_eq!(res, Vector::new([-1, 3, 2 / 3]));
+    }
+
+    #[test]
+    fn test_scalar_vector_division_assign() {
+        let mut vec1 = Vector::new([3, -3, 9]);
+        vec1 /= 3;
+
+        assert_eq!(vec1, Vector::new([-1, 3, 2 / 3]));
     }
 }
