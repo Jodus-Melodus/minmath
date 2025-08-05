e183a22648276da506630f784ed296d78e96b250
Jodus-Melodus <philipgricius@gmail.com>
2025-08-05 19:47:54 +0200

    venn init

--- src/probability.rs
+++ src/probability.rs
@@ -1,4 +1,24 @@
+use std::collections::HashMap;
+
+use crate::{Number, set_theory::Set};
+
 pub type Probability = f64;
+
+pub struct Venn<T: Number> {
+    sets: HashMap<&'static str, Set<T>>,
+}
+
+impl<T: Number> Venn<T> {
+    /// Creates a new Venn diagram of type `T`
+    pub fn new() -> Self {
+        Venn {
+            sets: HashMap::new(),
+        }
+    }
+
+    /// Add a `Set<T>` to the `Venn<T>`
+    pub fn add_set(&mut self, set_name: &'static str, set: Set<T>) {}
+}
 
 pub fn complement(p: Probability) -> Probability {
     1.0 - p
